e7d1d6374cbd2552b14be3701a5b5c25bcd78724
oratake <smith.shimomura@gmail.com>
2024-07-06 00:05:38 +0900

    仮commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,17 @@ import '@mantine/core/styles.css';
 import React from 'react';
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
-import { MantineProvider, ColorSchemeScript } from '@mantine/core';
+import {
+  MantineProvider,
+  ColorSchemeScript,
+  AppShell,
+  Burger,
+  Group,
+  Skeleton,
+} from '@mantine/core';
+import { useDisclosure } from '@mantine/hooks';
+import { RiAncientGateFill } from 'react-icons/ri';
+import { IconContext } from 'react-icons';
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -12,19 +22,47 @@ export const metadata: Metadata = {
   description: '檀家ツール',
 }
 
-export default function RootLayout({
+export default function Layout({
   children,
 }: {
   children: React.ReactNode
 }) {
+  // const [opened, { toggle }] = useDisclosure();
+
   return (
-    <html lang="en">
+    <html lang="ja">
       <head>
         <ColorSchemeScript />
       </head>
       <body className={inter.className}>
         <MantineProvider>
-          {children}
+          <AppShell
+            header={{ height: 80 }}
+            // navbar={{ width: 300, breakpoint: 'sm', collapsed: { mobile: !opened } }}
+            transitionDuration={0}
+            padding="md"
+          >
+            <AppShell.Header>
+              <Group h="100%" px="md">
+                <IconContext.Provider value={{ size: '32px' }}>
+                  <Burger opened={opened} onClick={toggle} hiddenFrom="sm" size="sm" />
+                  <RiAncientGateFill />
+                  <h1>Zenji -禅師-</h1>
+                </IconContext.Provider>
+              </Group>
+            </AppShell.Header>
+            <AppShell.Navbar p="md">
+              Navbar
+              {Array(15)
+                .fill(0)
+                .map((_, index) => (
+                  <Skeleton key={index} h={28} mt="sm" animate={false} />
+                ))}
+            </AppShell.Navbar>
+            <AppShell.Main>
+              {children}
+            </AppShell.Main>
+          </AppShell>
         </MantineProvider>
       </body>
     </html>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,9 @@
 'use client';
 
 import { useMemo, Suspense } from 'react';
-import { useDisclosure } from '@mantine/hooks';
 import {
-  AppShell,
-  Burger,
-  Group,
-  Skeleton,
   Table,
 } from '@mantine/core';
-import { RiAncientGateFill } from 'react-icons/ri';
-import { IconContext } from 'react-icons';
 
 import { useQuery, gql } from 'urql';
 import { UrqlProvider, ssrExchange, createClient, cacheExchange, fetchExchange } from '@urql/next';
@@ -41,55 +34,16 @@ export default function Home() {
 
     return [client, ssr];
   }, []);
-
-  const [opened, { toggle }] = useDisclosure();
-
   // const result = getClient().query(allDankasQueryDocument, {});
-
   return (
     <>
-      <AppShell
-        header={{ height: 80 }}
-        navbar={{ width: 300, breakpoint: 'sm', collapsed: { mobile: !opened } }}
-        transitionDuration={0}
-        padding="md"
-      >
-        <AppShell.Header>
-          <Group h="100%" px="md">
-            <IconContext.Provider value={{ size: '32px' }}>
-              <Burger opened={opened} onClick={toggle} hiddenFrom="sm" size="sm" />
-              <RiAncientGateFill />
-              <h1>Zenji -禅師-</h1>
-            </IconContext.Provider>
-          </Group>
-        </AppShell.Header>
-        <AppShell.Navbar p="md">
-          Navbar
-          {Array(15)
-            .fill(0)
-            .map((_, index) => (
-              <Skeleton key={index} h={28} mt="sm" animate={false} />
-            ))}
-        </AppShell.Navbar>
-        <AppShell.Main>
-          <h2>檀家一覧</h2>
-          <UrqlProvider client={client} ssr={ssr}>
-            <AddDanka />
-            <Suspense>
-              <TestList />
-            </Suspense>
-          </UrqlProvider>
-          {/*
-          {result.data &&
-            <ul>
-              {result.data.dankaModels?.map((x: any) => (
-                x?.danka_uuid && <li key={x.danka_uuid}>{x.last_name_of_family_head} {x.first_name_of_family_head}</li>
-              ))}
-            </ul>
-          }
-          */}
-        </AppShell.Main>
-      </AppShell>
+      <h2>檀家一覧</h2>
+      <UrqlProvider client={client} ssr={ssr}>
+        <AddDanka />
+        <Suspense>
+          <TestList />
+        </Suspense>
+      </UrqlProvider>
     </>
   );
 }
